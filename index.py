f6637bc6da702b8cb2852c18f477f9b4c7e2ea7b
ElenaTheAxolotl <elenavasquez0130@gmail.com>
2025-12-04 14:46:38 -0500

    welcome message, set name and score variables

---
+++ index.py
@@ -0,0 +1,9 @@
+#welcome message thingy yippee
+print("Hello, hello everyone!! Emphasis on the one, unless there are more of you, but that feels unlikely. Welcome, you one (I assume) contestant to the Amazing Axolotl Gameshow! Now, we ran out of mon- I mean this is a quizshow special, so there are no games. Not cut due to budget issues. Deffinetly didn't spend it all on rocks... ANYWAY, let's begin our quiz with the first question.")
+
+name = input("What is your name? ")
+
+print("INCORECT Just kidding! I don't know your acual name, so I can't say your answer was wrong. Enjoy the quiz," ,name,"!")
+
+#store score in varable and first question
+score = 0
