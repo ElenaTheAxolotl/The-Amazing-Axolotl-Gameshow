b289416e2c1e6197a7901468ddd79198d151baeb
ElenaTheAxolotl <elenavasquez0130@gmail.com>
2025-12-12 14:41:31 -0500

    fourth question and string for info blurb, formatting changes for questions two and three

--- index.py
+++ index.py
@@ -26,7 +26,7 @@ else:
 print("\nAlthough pink ones are the ones that have recieved the most attention, that color is a result of leucism and albinism. Axolotls are naturally dark to blend in with their natural habbitat, so brown is the most common color overall.\n")
 
 #second question yahoo
-print("\nLet's move on to the sec-OUK-AWKCKGH-EHemf...Pardon me, uh...QUESTION TWO:\n What is the lifespan of axolotls?")
+print("\nLet's move on to the sec-OUK-AWKCKGH-EHemf...Pardon me, uh...QUESTION TWO:\n What is the lifespan of axolotls?\n")
 print("A: 10-15 years")
 print("B: 6-8 years")
 print("C: 2-4 years")
@@ -39,10 +39,10 @@ if answerclean == "A":
 else:
     print("\nINCORRECT")
 
-print("\nAxolots usually live for 10-15 years like many other species of salamanders, and they spend their entire lives in the water despite developing lungs to-ERP...to breathe air.")
+print("\nAxolots usually live for 10-15 years like many other species of salamanders, and they spend their entire lives in the water despite developing lungs to-ERP...to breathe air.\n")
 
 #question threehehehehehehehe
-print("Moving right along to the third question:\n Which country are ACk-axolotls native to?...AHem...")
+print("\nMoving right along to the third question:\n Which country are ACk-axolotls native to?...AHem...\n")
 print("A: Mexico")
 print("B: Indonesia")
 print("C: Rwanda")
@@ -56,4 +56,20 @@ if answerclean == "A":
 else:
     print("\nCORRE-\nINCORRECT")
 
-print("\nMexico is the only native home of axolotls, specifically Lake Xochimilco in Mexico City. The Azetec god of fire, lightning, deformities, and death, Xolotl, is the origin of the name 'axolot', which means 'water dog' in Nahuatl.")
+print("\nMexico is the only native home of axolotls, specifically Lake Xochimilco in Mexico City. The Azetec god of fire and lightning, Xolotl, is the origin of the name 'axolot', which means 'water dog' in Nahuatl.\n")
+
+#fourthhhh questionnnnn
+print("\nSpeaking of names, it's time for the-URp-the fourth question:\n What is a common name for axolotls in Japan?\n")
+print("A: Rōtoru")
+print("B: Wooper Looper")
+print("C: Ahobaka")
+answer = input("\nYour answer, please (enter the letter): ")
+answerclean = answer.upper ()
+
+if answerclean == "B":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("\nINCORRECT")
+
+print("\nMaybe I shouldn't have eaten all those rocks...OH! UH-YOU KNOW NOTHING! AHEM! The term 'Wooper Looper' (ウーパールーパー) comes from the name of an axolotl character created to advertise UFO instant noodles and is the namesake for the axolotl-like Pokémon, Wooper. The transliteration of 'axolotl' in Japanese, ahorōtoru (アホロートル), is the same as saying 'stupid old fart', so Wooper Looper is a much nicer name.\n")
