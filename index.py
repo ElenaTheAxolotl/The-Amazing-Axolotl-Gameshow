6962977dc867a5d9dc1ee1f913648e53c84dc6f2
ElenaTheAxolotl <elenavasquez0130@gmail.com>
2025-12-12 13:23:43 -0500

    second quetion and string for info blurb, fixed score addition, minor wording change in answer prompt

--- index.py
+++ index.py
@@ -14,13 +14,29 @@ print("We'll start with something fairly simple:\n What is the most common color
 print("A: Yellow")
 print("B: Pink")
 print("C: Brown")
-answer = input("\nYour answer (enter the letter): ")
+answer = input("\nYour answer, please (enter the letter): ")
 answerclean = answer.upper ()
 
 if answerclean == "C":
     print("\nCORRECT!")
-    score =+ 1
+    score += 1
 else:
     print("\nINCORRECT")
 
-print("\nAlthough pink ones are the ones that have recieved the most attention, that color is a result of leucism and albinism. Axolotls are naturally dark to blend in with their natural habbitat, so brown is the most common color overall.")
+print("\nAlthough pink ones are the ones that have recieved the most attention, that color is a result of leucism and albinism. Axolotls are naturally dark to blend in with their natural habbitat, so brown is the most common color overall.\n")
+
+#second question yahoo
+print("\nLet's move on to the sec-OUK-AWKCKGH-EHemf...Pardon me, uh...QUESTION TWO:\n What is the lifespan of axolotls?")
+print("A: 10-15 years")
+print("B: 6-8 years")
+print("C: 2-4 years")
+answer = input("\nYour answer, please (enter the letter): ")
+answerclean = answer.upper ()
+
+if answerclean == "A":
+    print("\nCORRECT!")
+    score += 1
+else:
+    print("\nINCORRECT")
+
+print("\nAxolots usually live for 10-15 years like many other species of salamanders, and they spend their entire lives in the water despite developing lungs to-ERP...to breathe air.")
